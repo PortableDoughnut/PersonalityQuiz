c8dc146d11cdd4b9c08d49bf86c3a3a18b6030c4
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-01 13:21:52 -0600

    Set up the enums and structs for the questions and I wrote my first question.

---
+++ PersonalityQuiz/Question.swift
@@ -0,0 +1,62 @@
+//
+//  Question.swift
+//  PersonalityQuiz
+//
+//  Created by Gwen Thelin on 10/1/24.
+//
+
+import Foundation
+
+struct Question {
+	var text: String
+	var type: ResponceType
+	var answer: [Answer]
+}
+
+enum ResponceType {
+	case single, multiple, ranged
+}
+
+struct Answer {
+	var text: String
+	var type: Line
+}
+
+enum Line: Character {
+	case red = "🟥"
+	case green = "🟩"
+	case yellow = "🟨"
+	case blue = "🟦"
+	case purple = "🟪"
+	case brown = "🟫"
+	case pink = "🩷"
+	
+	var string: String {
+		switch self {
+			case .red: return "Red"
+			case .green: return "Green"
+			case .yellow: return "Yellow"
+			case .blue: return "Blue"
+			case .purple: return "Purple"
+			case .brown: return "Brown"
+			case .pink: return "Pink"
+		}
+	}
+	
+	var definition: String {
+		switch self {
+			//TODO
+			case .red: return "Red"
+			case .green: return "Green"
+			case .yellow: return "Yellow"
+			case .blue: return "Blue"
+			case .purple: return "Purple"
+			case .brown: return "Brown"
+			case .pink: return "Pink"
+		}
+	}
+}
+
+var questions: [Question] = [
+	Question(text: "How long do you like to spend on the train?", type: .ranged, answer: [Answer(text: "I don't ride the train", type: .yellow), Answer(text: "I like to spend a little but of time on the train", type: .pink), Answer(text: "I like to spend a moderate amount of time on the train", type: .brown), Answer(text: "I spend a lot of time on the train and it's great", type: .blue)])
+]
